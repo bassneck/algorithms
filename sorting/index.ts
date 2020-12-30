f4d8193aa9f18a161a411e749080b5df34dc58f1
Constantine Zaytsev <bassneck@gmail.com>
2020-12-30 14:38:51 +1300

    Reduce the number of iterations
    The last i elements should be in the right position after every run

--- sorting/index.ts
+++ sorting/index.ts
@@ -2,7 +2,7 @@ export function bubbleSort(array: any[]): any[] {
   const result = [...array]
 
   for (let i = 0; i < result.length; i++) {
-    for (let j = 0; j < result.length; j++) {
+    for (let j = 0; j < result.length - i; j++) {
       if (result[j] > result[j + 1]) {
         const temp = result[j + 1]
         
