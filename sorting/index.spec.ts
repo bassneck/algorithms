dbe716bd6b2f03b3f216f185acf3b788b961600f
Constantine Zaytsev <bassneck@gmail.com>
2020-12-30 14:40:42 +1300

    Reduce the number of iterations on pre-sorted arrays

--- sorting/index.spec.ts
+++ sorting/index.spec.ts
@@ -3,6 +3,7 @@ import { bubbleSort } from './index'
 describe.each([
   [[1, 2], [1, 2]],
   [[2, 1], [1, 2]],
+  [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
   [[23, 42, 8, 15, 4, 16], [4, 8, 15, 16, 23, 42]]
 ])('bubbleSort(%o)', (a, expected) => {
   it(`returns ${expected}`, () => {
--- sorting/index.ts
+++ sorting/index.ts
@@ -1,6 +1,8 @@
 export function bubbleSort(array: any[]): any[] {
   const result = [...array]
 
+  let swapped = false
+
   for (let i = 0; i < result.length; i++) {
     for (let j = 0; j < result.length - i; j++) {
       if (result[j] > result[j + 1]) {
@@ -8,7 +10,13 @@ export function bubbleSort(array: any[]): any[] {
         
         result[j + 1] = result[j]
         result[j] = temp
+
+        swapped = true
       }
+    }
+
+    if (!swapped) {
+      break
     }
   }
 
