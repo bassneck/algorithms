d00040f9177a448b014b49bb82722631d1edb46f
Constantine Zaytsev <bassneck@gmail.com>
2020-12-30 14:36:37 +1300

    Implement simple bubblesort

---
+++ sorting/index.spec.ts
@@ -0,0 +1,11 @@
+import { bubbleSort } from './index'
+
+describe.each([
+  [[1, 2], [1, 2]],
+  [[2, 1], [1, 2]],
+  [[23, 42, 8, 15, 4, 16], [4, 8, 15, 16, 23, 42]]
+])('bubbleSort(%o)', (a, expected) => {
+  it(`returns ${expected}`, () => {
+    expect(bubbleSort(a)).toEqual(expected)
+  })
+})
---
+++ sorting/index.ts
@@ -0,0 +1,16 @@
+export function bubbleSort(array: any[]): any[] {
+  const result = [...array]
+
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] > result[j + 1]) {
+        const temp = result[j + 1]
+        
+        result[j + 1] = result[j]
+        result[j] = temp
+      }
+    }
+  }
+
+  return result
+}
